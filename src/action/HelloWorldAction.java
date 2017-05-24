5827a8253538af7dcada2b558778f398ef0c3d83
linghaoxinpian <530060499@qq.com>
2017-05-24 18:32:20 +0800

    struts2
    struts2系列教程

---
+++ src/action/HelloWorldAction.java
@@ -0,0 +1,19 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import model.MessageStore;
+
+public class HelloWorldAction extends ActionSupport {
+    private MessageStore messageStore;
+
+    public String execute() {
+        messageStore = new MessageStore() ;
+        
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+}
